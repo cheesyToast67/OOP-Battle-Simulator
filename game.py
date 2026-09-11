e132714ed91068341b3110410510c85680f3bc5a
cheesyToast67 <elias.burkett@ascte.org>
2026-09-11 10:23:14 -0500

    Added Bob
    He enters the arena with aura.

--- game.py
+++ game.py
@@ -11,8 +11,11 @@ def main():
     print("The gates are opening...")
 
     goblin = Goblin("Remmy Crusher of Skulls and Emperor of Power")
-
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+
+    goblin2 = Goblin("Bob")
+    print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
+
     print("But no hero has answered the call... yet.")
 
 
