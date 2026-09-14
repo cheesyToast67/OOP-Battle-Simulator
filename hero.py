7b8f487296bff6871438eedf601876eda2a8c5e0
cheesyToast67 <elias.burkett@ascte.org>
2026-09-14 15:08:57 -0500

    hotfix

--- hero.py
+++ hero.py
@@ -1,12 +1,15 @@
+
+import random 
+
 class Hero:
-    def __init__(self, name, health, attack_power, armor_power):
+    def __init__(self, name, armor_power):
         self.name = name
-        self.health = health
-        self.attack_power = attack_power
+        self.health = random.randint(100,150)
+        self.attack_power = random.randint(10,25)
         self.armor_power = armor_power
 
     def attack(self):
-        return self.attack_power
+        return random.randint(1, self.attack_power)
 
     def take_damage(self, damage):
         damage = damage - self.armor_power
